6cd0ed1c0eb5e8ec959bd92ba173fc631065fcfe
ys111222 <3165762054@qq.com>
2020-10-25 13:06:43 +0800

    修改了文件222

--- src/main/java/com/atguigu/Love.java
+++ src/main/java/com/atguigu/Love.java
@@ -2,6 +2,6 @@ package com.atguigu;
 
 public class Love {
     public static void main(String[] args) {
-        System.out.println("1111 = " + 1111);
+        System.out.println("1111 = " + 1111222);
     }
 }
