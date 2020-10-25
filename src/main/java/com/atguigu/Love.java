96ab6bbc40e6580590489839a59e264c6f459f5a
ys111222 <3165762054@qq.com>
2020-10-25 13:29:25 +0800

    增加了几行代码

--- src/main/java/com/atguigu/Love.java
+++ src/main/java/com/atguigu/Love.java
@@ -4,5 +4,8 @@ public class Love {
     public static void main(String[] args) {
         System.out.println("1111 = " + 1111222);
         System.out.println("1111 = " + 1111333);
+        System.out.println("1111 = " + 1111333);
+        System.out.println("1111 = " + 1111333);
+        System.out.println("1111 = " + 1111333);
     }
 }
